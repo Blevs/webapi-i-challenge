672e21bdcba0e914c8f7a15d6a4931b332cf9a20
Henry Blevins <heblevi@gmail.com>
2019-04-22 16:31:08 -0400

    Install express and implement get users route

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const db = require('./data/db.js');
+const express = require('express');
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(error => res.status(500).json({error}));
+});
+
+server.listen(5000, () => {
+    console.log('sever running on port 5000');
+});
+
