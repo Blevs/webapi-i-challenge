f1a467acca73c527f62f3d4d3b8fc9befcd75084
Henry Blevins <heblevi@gmail.com>
2019-04-22 16:35:00 -0400

    Get user by id

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const server = express();
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => res.status(200).json(users))
-    .catch(error => res.status(500).json({error}));
+    .catch(error => res.status(500).json({error: "The users information could not be retrieved."}));
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => user
+          ? res.status(200).json(user)
+          : res.status(404).json({error: "The user with the specified ID does not exist."}))
+    .catch(error => res.status(500).json({error: "The user information could not be retrieved."}));
 });
 
 server.listen(5000, () => {
