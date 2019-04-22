d58fb0abf5eb86fa329bd98b3dec5848a898c50c
Henry Blevins <heblevi@gmail.com>
2019-04-22 18:10:02 -0400

    Hookup Home component with route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
-      app
+      <Route exact path="/" component={Home}/>
     </div>
   );
 }
