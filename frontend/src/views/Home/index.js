203e54e9ed25086073241a5e21bbc34214c9fd9e
Henry Blevins <heblevi@gmail.com>
2019-04-22 18:56:25 -0400

    Implement deletion

--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -9,17 +9,27 @@ const Home = (props) => {
   useEffect(() => {
     api.getUsers()
       .then(res => setUsers(res.data))
-      .catch(error => setError(Error));
+      .catch(error => setError(error.response.data.error));
   }, [props.history]);
+  const handleDelete = id => {
+    api.deleteUser(id)
+      .then(res => api.getUsers()
+            .then(res => setUsers(res.data))
+            .catch(error => setError(error.response.data.error)))
+      .catch(error => setError(error.response.data.error));
+  };
   return (
     <div className="home">
       Home
+      {error && <div>{error}</div>}
       <Card.Group>
         {users.map(user => (
           <Card key={user.id}>
             <Card.Meta style={{display: "flex", justifyContent: "space-between", padding: "0 10px"}}>
               <Link className="edit" to={`/edit/${user.id}`}>edit</Link>
-              <span className="delete">delete</span>
+              <span className="delete"
+                    onClick={() => handleDelete(user.id)}>
+                delete</span>
             </Card.Meta>
             <Card.Content>
               <Card.Header>{user.name}</Card.Header>
